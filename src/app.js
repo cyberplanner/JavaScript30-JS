1e9374aad84fd225b463a61266189e5a44d73b11
cyberplanner <sal@salweb.co.uk>
2016-12-14 02:32:08 +0000

    Add CSS

--- src/app.js
+++ src/app.js
@@ -1,7 +1,14 @@
-window.addEventListener('keydown', function(e) {
+window.addEventListener('keydown', function(e) { //will catch the key number
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  console.log(audio);
+  const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+  console.log(e); //log Keyboard event
+  //console.log(e.keyCode); //number of the key
+  //console.log("Audio element: ");
+  //console.log(audio);
+  //console.log("Key element: ");
+  //console.log(key);
   if (!audio) return; //will stop the function from running
-  audio.currentTime = 0;
+  audio.currentTime = 0;// rewind audio
   audio.play();
+  key.classList.add('playing');
 })
